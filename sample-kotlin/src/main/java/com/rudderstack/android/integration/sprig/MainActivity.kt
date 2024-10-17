75464f82c0d7cff87b54b8736ce89910c5985912
Vishal Gupta <vishal@rudderstack.com>
2024-10-17 20:36:18 +0530

    feat: add sprig integration

---
+++ sample-kotlin/src/main/java/com/rudderstack/android/integration/sprig/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.rudderstack.android.integration.sprig;
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.rudderstack.android.sdk.core.RudderProperty
+import com.rudderstack.android.sdk.core.RudderTraits
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        SprigIntegrationFactory.FACTORY.setCurrentFragmentActivity(this@MainActivity)
+
+        findViewById<Button>(R.id.identify_button).setOnClickListener {
+            MainApplication.rudderClient.identify(
+                "test_user_id",
+                RudderTraits()
+                    .putEmail("test@gmail.com")
+                    .put("v1", 1)
+                    .put("v2", "2"),
+                null
+            )
+        }
+
+        findViewById<Button>(R.id.track_button).setOnClickListener {
+            MainApplication.rudderClient.track(
+                "test_event"
+            )
+        }
+
+        findViewById<Button>(R.id.track_properties_button).setOnClickListener {
+            MainApplication.rudderClient.track(
+                "test_event_with_properties",
+                RudderProperty()
+                    .putValue("key_1", "value_1")
+                    .putValue("key_2", "value_2")
+            )
+        }
+    }
+}
